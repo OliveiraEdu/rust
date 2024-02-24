abc98a402ab91d164f637ea23813ecdedf700211
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-02-24 19:37:57 -0300

    update Cargo.toml and main.rs

--- projects/ipfs_client/src/main.rs
+++ projects/ipfs_client/src/main.rs
@@ -1,16 +1,15 @@
-use ipfs_api::{IpfsApi, IpfsClient};
+use ipfs_api::{IpfsClient};
 use std::io::Cursor;
 use actix_rt::Runtime;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     // Replace "http://localhost:5001" with your actual IPFS node address
     let client = IpfsClient::new("http://localhost:5001");
     let data = Cursor::new("Hello World!");
 
     let rt = Runtime::new()?;
 
-    let res = rt.block_on(client.add(data)).await?;
+    let res = rt.block_on(client.add(data))?;
 
     println!("{}", res.hash);
 
