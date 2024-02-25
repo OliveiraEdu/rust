47d9d02e613ec5ead788b79258e2a17195dd1673
OliveiraEdu <eduardocostaoliveira@gmail.com>
2024-02-25 12:17:04 -0300

    update main.rs

--- projects/ipfs_client/src/main.rs
+++ projects/ipfs_client/src/main.rs
@@ -1,9 +1,14 @@
-use ipfs_api::{IpfsApi, IpfsClient};
+use ipfs_api::{IpfsApi, IpfsClient, Scheme};
 use std::io::Cursor;
 
 #[actix_rt::main]
 async fn main() {
-    let client = IpfsClient::default();
+    let client = IpfsClient::from_host_and_port(
+        Scheme::Http,
+        "192.168.1.100",
+        5001
+    ).expect("Failed to create IPFS client");
+
     let data = Cursor::new("Hello World!");
 
     match client.add(data).await {
